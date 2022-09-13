3173554f41d51e9219f794846d07c9d3c8ef01cc
shamel jij <shamel.jij@gmail.com>
2022-09-13 12:13:55 +0200

    second commit

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 class Someparent {
-    constructor(name, age){
-        this.age;
-        this.name;
-    }
-    stateName(someStr){
-        //change feld!!
-        $("#feld").text(someStr);
-    }
+  constructor(name, age) {
+    this.age;
+    this.name;
+  }
+  stateName(someStr) {
+    //change feld!!
+    $("#feld").text(someStr);
+  }
 }
 
-class SomeChildClass extends Someparent{
-    constructor(){
-        super();
-    }
+class SomeChildClass extends Someparent {
+  constructor() {
+    super();
+  }
 }
-let child = new SomeChildClass;
+let child = new SomeChildClass();
 child.stateName("this is a test");
 //$("#feld").text("sometxt");
+//newcontent
