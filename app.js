39b1068f0e5240fb494d490be8ca57afeb3b51c8
shamel jij <shamel.jij@gmail.com>
2022-10-05 14:40:50 +0200

    added blinker with counter

--- app.js
+++ app.js
@@ -5,7 +5,32 @@ class Someparent {
   }
   stateName(someStr) {
     //change feld!!
-    $("#feld").text(someStr);
+    $('#feld').text(someStr);
+  }
+  showMatrix(first, second, third) {
+    let i = first;
+    let j = second;
+    let k = third;
+    $(document).ready(function () {
+      var f = document.getElementById('col2');
+      setInterval(function () {
+        if (i > 99990) {
+          return;
+        } else {
+          f.style.display = f.style.display == 'none' ? '' : 'none';
+          i *= 2;
+          j *= 3;
+          k *= 4;
+          $('#col2').append(
+            `<p class="text-center bg-danger h4 text-white p-3">i = ${i} | j = ${j} | k = ${k}</p>`
+          );
+          setTimeout(function () {
+            $('#col2').empty();
+          }, 600);
+          console.log(f.text);
+        }
+      }, 800);
+    });
   }
 }
 
@@ -15,7 +40,6 @@ class SomeChildClass extends Someparent {
   }
 }
 let child = new SomeChildClass();
-child.stateName("this is a test");
+child.stateName('this is a test');
+child.showMatrix(1, 2, 3);
 //$("#feld").text("sometxt");
-//newcontent
-//someothercontent
