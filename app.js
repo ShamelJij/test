9491b0e3b1ca521d422cec2ff5e2b20de45efbca
shamel jij <shamel.jij@gmail.com>
2022-10-07 09:44:01 +0200

    multiple promises with nested promises in them are working now

--- app.js
+++ app.js
@@ -43,6 +43,109 @@ class Someparent {
       }, 600);
     });
   }
+  async anotherPromise(someParam) {
+    let theEnd = someParam;
+    let nndx1 = 0;
+    let nndx2 = 0;
+    let nndx3 = 0;
+    const somePromise = await new Promise((resolve, reject) => {
+      setInterval(function () {
+        if (nndx1 > theEnd) {
+          resolve();
+        } else {
+          nndx1++;
+          //   d.style.display = d.style.display == 'none' ? '' : 'none';
+          $('#col11').append(
+            `<p class="text-center round bg-success small text-white">result = ${nndx1}</p>`
+          );
+          console.log(nndx1);
+        }
+      }, 200);
+      console.log('nndx1');
+    });
+    const somePromise1 = await new Promise((resolve, reject) => {
+      async function nestedPromise() {
+        let theEnd1 = 6;
+        let nndx23 = 0;
+        if (nndx23 > theEnd1) {
+          resolve();
+          console.log('resolved');
+        } else {
+          //   d.style.display = d.style.display == 'none' ? '' : 'none';
+          let nndx12 = 0;
+          let nndx22 = 0;
+          const somePromise21 = await new Promise((resolve, reject) => {
+            setInterval(function () {
+              if (nndx12 > theEnd1) {
+                resolve();
+              } else {
+                nndx12++;
+                //   d.style.display = d.style.display == 'none' ? '' : 'none';
+                $('#col21').append(
+                  `<p class="text-center round bg-success small text-white">col21 = ${nndx12}</p>`
+                );
+                console.log(nndx12);
+              }
+            }, 200);
+            console.log('nndx12');
+          });
+          const somePromise22 = await new Promise((resolve, reject) => {
+            setInterval(function () {
+              if (nndx22 > theEnd1) {
+                resolve();
+              } else {
+                nndx22++;
+                //   d.style.display = d.style.display == 'none' ? '' : 'none';
+                $('#col22').append(
+                  `<p class="text-center round bg-success small text-white">col22 = ${nndx22}</p>`
+                );
+                console.log(nndx22);
+              }
+            }, 200);
+            console.log('nndx22');
+          });
+          const somePromise23 = await new Promise((resolve, reject) => {
+            setInterval(function () {
+              if (nndx23 > theEnd1) {
+                resolve();
+              } else {
+                nndx23++;
+                //   d.style.display = d.style.display == 'none' ? '' : 'none';
+                $('#col23').append(
+                  `<p class="text-center round bg-success small text-white">col23 = ${nndx23}</p>`
+                );
+                console.log(nndx23);
+              }
+            }, 200);
+          });
+          //   $('#col12').append(
+          //     `<p class="text-center round bg-success small text-white">result = ${nndx2}</p>`
+          //   );
+          resolve();
+          console.log('resolve after the third nested nndx23');
+          console.log(nndx23);
+        }
+      }
+      nestedPromise();
+      console.log('nndx2');
+    });
+    const somePromise2 = await new Promise((resolve, reject) => {
+      setInterval(function () {
+        if (nndx3 > theEnd) {
+          resolve();
+        } else {
+          nndx3++;
+          //   d.style.display = d.style.display == 'none' ? '' : 'none';
+          $('#col13').append(
+            `<p class="text-center round bg-success small text-white">result = ${nndx3}</p>`
+          );
+          console.log(nndx3);
+        }
+      }, 200);
+      console.log('nndx3');
+    });
+  }
+
   async promiseAllDemo(endTimePoint) {
     try {
       let theEnd = endTimePoint;
@@ -58,7 +161,7 @@ class Someparent {
             let theEndHex1 = ndx1.toString(16);
             //   d.style.display = d.style.display == 'none' ? '' : 'none';
             $('#col1').append(
-              `<p class="text-center round bg-info small text-white">Decimal = ${ndx1} | Hexadecimal = ${theEndHex1}</p>`
+              `<p class="text-center round bg-success small text-white">Decimal = ${ndx1} | Hexadecimal = ${theEndHex1}</p>`
             );
             console.log(ndx1);
           }
@@ -74,11 +177,11 @@ class Someparent {
             let theEndHex2 = ndx2.toString(16);
             //   d.style.display = d.style.display == 'none' ? '' : 'none';
             $('#col2').append(
-              `<p class="text-center round bg-info small text-white">Decimal = ${ndx2} | Hexadecimal = ${theEndHex2}</p>`
+              `<p class="text-center rounded bg-info py-2 small text-white">Decimal = ${ndx2} | Hexadecimal = ${theEndHex2}</p>`
             );
             console.log(ndx2);
           }
-        }, 200);
+        }, 600);
         console.log('sec');
       });
       const promise3 = await new Promise((resolve, reject) => {
@@ -90,7 +193,7 @@ class Someparent {
             let theEndHex3 = ndx3.toString(16);
             //   d.style.display = d.style.display == 'none' ? '' : 'none';
             $('#col3').append(
-              `<p class="text-center round bg-info small text-white">Decimal = ${ndx3} | Hexadecimal = ${theEndHex3}</p>`
+              `<p class="text-center round bg-danger small text-white">Decimal = ${ndx3} | Hexadecimal = ${theEndHex3}</p>`
             );
             console.log(ndx3);
           }
@@ -118,4 +221,5 @@ child.stateName('this is a test');
 // child.showMatrix(1, 2, 3);
 // child.showTimer(14);
 child.promiseAllDemo(14);
+child.anotherPromise(4);
 //$("#feld").text("sometxt");
